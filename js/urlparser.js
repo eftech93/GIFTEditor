7fb747b41a3c5d131641884eea9b786fc1ee0249
Tebo <esteban.puello1@gmail.com>
2018-06-02 18:32:55 -0400

    Creation du formulaire
    Dans cette version, on a ajouté les librairies jquery, urlparser, pegjs et le handler de la page index.
    
    UserStories :
    
    US-13

---
+++ js/urlparser.js
@@ -0,0 +1,9 @@
+//got from : https://stackoverflow.com/questions/19491336/get-url-parameter-jquery-or-how-to-get-query-string-values-in-js
+$.urlParam = function (name) {
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    if (results == null) {
+        return undefined;
+    } else {
+        return decodeURI(results[1]) || 0;
+    }
+}
